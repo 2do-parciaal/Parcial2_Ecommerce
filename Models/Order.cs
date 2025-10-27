dcc520abfb8621e87ea21c4c3b3b2818df9cfe40
Samy Saed <a2024111190@estudiantes.upsa.edu.bo>
2025-10-27 19:55:59 -0400

    Se agrego modelos al proyecto final

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+namespace Parcial2_Ecommerce.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsPaid { get; set; } = false;
+
+        public ICollection<OrderItem>? Items { get; set; }
+    }
+}
